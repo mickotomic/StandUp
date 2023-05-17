edd832902577004ae09be22a5f317b7e66735820
mickotomic <milorad.tomic.87@gmail.com>
2023-05-17 08:40:13 +0200

    Update .env, .env.dist, and 5 more files...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('StandUp')
+    .addBearerAuth()
+    .setDescription('Platform for meetings!')
+    .setVersion('1.0')
+    .addTag('stand-up')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
