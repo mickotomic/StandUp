27ab084e237d2fa7b4043d1e384727eae3d4ce5c
DrugBoban <boban_bn@hotmail.com>
2023-06-22 18:01:18 +0200

    Update task.module.ts

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -3,9 +3,10 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from 'src/entities/task.entity';
+import { UserWorkspace } from 'src/entities/user-workspace.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task, UserWorkspace])],
   controllers: [TaskController],
   providers: [TaskService],
 })
