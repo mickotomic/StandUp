7e5b0e251887215f9666385f9ec4ab182f0c7c9b
Bane <bane.popic1998@gmail.com>
2023-07-11 21:01:38 +0200

    Update subscription.controller.ts

--- src/app/subscription/subscription.controller.ts
+++ src/app/subscription/subscription.controller.ts
@@ -3,7 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { SubscriptionService } from './subscription.service';
 
 @ApiTags('app-subscriptions')
-@Controller('subscription')
+@Controller('subscriptions')
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
 }
