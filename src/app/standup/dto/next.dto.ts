6fc3eba2a9e41f3115bb10e9ef2b4fe637110d6a
Bane <bane.popic1998@gmail.com>
2023-08-24 19:00:44 +0200

    Update next.dto.ts

--- src/app/standup/dto/next.dto.ts
+++ src/app/standup/dto/next.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsIn } from 'class-validator';
 
 export class NextDto {
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: string })
   @IsString()
   @IsIn([ 'next', 'previous' ])
   direction: string;
