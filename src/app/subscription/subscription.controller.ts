865ffa4ba464fb0d04bc8409269b5b3eaf7c0311
Bane <bane.popic1998@gmail.com>
2023-09-26 19:33:50 +0200

    app subscription

--- src/app/subscription/subscription.controller.ts
+++ src/app/subscription/subscription.controller.ts
@@ -1,9 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
 import { SubscriptionService } from './subscription.service';
 
 @ApiTags('app-subscriptions')
 @Controller('subscriptions')
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
+  // @UseGuards(WorkspaceOwner)
+  @Get('/:workspaceId')
+  async getWorkspaceSubscriptions(
+    @Param('workspaceId') workspaceId: string,
+    @Paginate() query: PaginateQuery,
+  ) {
+    return await this.subscriptionService.getWorkspaceSubscriptions(
+      +workspaceId,
+      query,
+    );
+  }
 }
--- src/app/subscription/subscription.service.ts
+++ src/app/subscription/subscription.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import {
+  paginate,
+  PaginateConfig,
+  Paginated,
+  PaginateQuery,
+} from 'nestjs-paginate';
 import { Subscription } from 'src/entities/subscription.entity';
 import { Repository } from 'typeorm';
 
@@ -9,4 +15,21 @@ export class SubscriptionService {
     @InjectRepository(Subscription)
     private subscriptionRepository: Repository<Subscription>,
   ) {}
+
+  async getWorkspaceSubscriptions(
+    workspaceId: number,
+    query: PaginateQuery,
+  ): Promise<Paginated<Subscription>> {
+    const paginateConfig: PaginateConfig<Subscription> = {
+      defaultLimit: 50,
+      sortableColumns: ['id'],
+      relations: [],
+      defaultSortBy: [['id', 'DESC']],
+    };
+    const qb = this.subscriptionRepository
+      .createQueryBuilder('subscriptions')
+      .leftJoinAndSelect('subscriptions.workspace', 'workspace')
+      .where('workspace.id = :workspaceId', { workspaceId });
+    return await paginate<Subscription>(query, qb, paginateConfig);
+  }
 }
